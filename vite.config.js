d71fa15cdfebfe92e85c89134dd4c99c29b1261a
van-sta-n <115972816+vow-sta-n@users.noreply.github.com>
2026-09-22 12:28:54 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/van-stan-folio/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-});
+})
